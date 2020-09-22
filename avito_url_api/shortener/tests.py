368ae36e1b40c5593fce4e619ab2e4961d7be59f
yellowpearl <yellowpearl@yandex.ru>
2020-09-22 16:15:38 +0300

    prototype

--- avito_url_api/shortener/tests.py
+++ avito_url_api/shortener/tests.py
@@ -1,5 +1,27 @@
 from django.test import TestCase
 import requests
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_upper(self):
+      self.assertEqual('foo'.upper(), 'FOO')
+
+  def test_isupper(self):
+      self.assertTrue('FOO'.isupper())
+      self.assertFalse('Foo'.isupper())
+
+  def test_split(self):
+      s = 'hello world'
+      self.assertEqual(s.split(), ['hello', 'world'])
+      # Проверим, что s.split не работает, если разделитель - не строка
+      with self.assertRaises(TypeError):
+          s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
+
 headers = {'User-Agent': 'My User Agent 1.0',}
 
 client = requests.session()
@@ -7,6 +29,6 @@ r = requests.post('http://127.0.0.1:8000/s/short', data={'url':'piepi.com'}, hea
 print(r.text)
 #print(r.text[r.text.index('=')+1:])
 #print(requests.post('http://127.0.0.1:8000/s/short', data={'key':'vk.com'}, headers=headers).text)
-print(requests.get('http://127.0.0.1:8000/s/wewe'))
+print(requests.get('http://127.0.0.1:8000/s/wewe').text)
 print(requests.get('http://127.0.0.1:8000/s/google'))
 print(requests.get('http://127.0.0.1:8000/s/vkcom'))
